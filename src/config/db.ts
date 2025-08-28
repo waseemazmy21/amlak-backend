be8325e52f9bdc662a3ed612bfb1d8e0d2d025f3
waseem azmy <waseemazmy21@gmail.com>
2025-08-28 23:21:22 +0300

    fix deployment

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI;
     if (!uri) {
-      throw new Error('MONGODB_URI is not defined in environment variables');
+      throw new Error('MONGO_URI is not defined in environment variables');
     }
     await mongoose.connect(uri);
     console.log('[MongoDB] connected successfully');
